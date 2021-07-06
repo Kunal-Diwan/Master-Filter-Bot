7938aa7c1f859cea84637fe8a93075a4905c83a4
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-07-06 20:28:31 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,76 @@
+class Script(object):
+
+    START_MSG = """<b>Heya {},
+
+I'm Master filter bot with many capabilities!
+
+I can help you to set unlimited filters :)
+
+Hit /help for commands and details.
+
+Powered by @DevelopedBots 🔔</b>
+"""
+
+
+    HELP_MSG = """
+<b>Add me as admin in your group and start filtering :)</b>
+
+
+<b>Basic Commands ⬇️</b>
+
+/start - Check that I'm alive!
+
+/help - My commands help
+
+/about - Something about me!
+
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+
+<b><u>More commands ➡️</u></b>
+
+<b>Filter Commands ➖</b>
+
+<code>/add name reply</code>  👉  Add filter for name .
+
+<code>/del name</code>  👉  Delete filter .
+
+<code>/delall</code>  👉  Delete entire filters (Group Owner Only) .
+
+<code>/viewfilters</code>  👉  List all filters in chat .
+
+<b>Connection Commands ➖</b>
+
+<code>/connect groupid</code>  👉  Connect your group to my PM. You can also simply use .
+
+<code>/connect</code> 👉 send me in group to connect directly. 
+
+<code>/connections</code>  👉  Manage your connections .
+
+<b>Extras ➖ </b>
+
+/status  👉  Shows current status of your bot (Auth User Only)
+
+/id  👉  Shows ID information
+
+<code>/info userid</code>  👉  Shows User Information. Use <code>/info</code> as reply to some message for their details!
+
+
+<b>Developed by @kunaldiwan 👨‍💻 .</b>
+"""
+
+
+    ABOUT_MSG = """🤖 <b>My Name : Master Filter Bot</b>
+
+👨‍💻 <b>Creater :</b> @kunaldiwan 
+
+🏳‍🌈 <b>Language :</b> <code>Python3</code>
+
+📚 <b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+<b><u>I am powerfull telegram filter bot. I am developed in python3 by [kunal](https://t.me/kunaldiwan) . I am bot in which you can set infinity filters.</u></b>
+
+<b><u>There's my source code - [click here](https://github.com/DevelopedBots/Master-Filter-Bot) .</u></b>
+
+<b>👆 That's all my about 👆</b>
+
+"""
